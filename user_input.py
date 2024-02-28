21b27a541a5144b1e9eab08164f1834bd9f2d996
Kim-mkuu <kimukuphilip10@gmail.com>
2024-02-28 21:37:06 +0300

    added user_input.py

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#Ask the user for their name
+name = input("Enter Your Name: ")
+
+#Ask user for their age
+age = input("Enter Your Age: ")
+
+#Ask user for their location
+location = input("Enter Your Location: ")
+
+#Print out a personalized message
+print(f"Hello {name},you are{age}, years old and live in{location}.")
